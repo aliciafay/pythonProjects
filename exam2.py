5e3a5185846a605b11023cadccdd056d9a10190d
Alicia Leslie <aliciafleslie@gmail.com>
2014-09-09 13:04:07 -0700

    Delete exam2.py

--- exam2.py
+++
@@ -1,7 +0,0 @@
-num=int(input("enter a number"))
-num2=int(input("enter a number"))
-print("your number is",num)
-print("your number is", num2)
-while num!=-999:
-    num=int(input("enter a number or -999 to quit"))
-    print("your number is",num)
